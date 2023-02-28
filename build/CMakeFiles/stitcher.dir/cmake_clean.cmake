63a377dd1e91e3fbb8e6332dc3dc9b90f7e5aca0
yujin3 <yujin3@xiaomi.com>
2023-02-28 21:44:04 +0800

    feat-dev-multi-fisheye-stiching

---
+++ build/CMakeFiles/stitcher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stitcher.dir/src/fisheye_stitcher.cpp.o"
+  "libstitcher.pdb"
+  "libstitcher.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stitcher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
